9febf5a2681fb9a4066dc5b55c5d0f5cec13e889
lakaszleni <lakasz.helena@students.jedlik.eu>
2022-11-24 12:38:57 +0100

    mainből átraktam a menüt functionsba

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 from data import nevek, betegségek
-from os import system
-fajlnev='data.csv'
 
-def menu():
-    system('cls')
-    print('~~~~~MENÜ~~~~~')
-    print('0 - Kilépés')
-    print('1 - Betegek nevei')
-    print('2 - Betegségük')
-    print('3 - Új beteg felvétele')
-    print('4 - Beteg törlése')
-    print('5 - Leghosszebb ideig bent lévő beteg')
-    print('6 - Legkevesebb ideig bent lévő beteg')
-    return input('Választás: ')
+
